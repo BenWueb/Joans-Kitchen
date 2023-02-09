31c938b031b4160c311dec9151a3ba7363ff84ff
Ben Wuebker <yallsobad@protonmail.com>
2023-02-08 19:12:04 -0600

    changed homepage button to render based on auth

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,8 +4,10 @@ import MobileNavbar from "../components/MobileNavbar";
 
 import CategorySection from "../components/CategorySection";
 import AddRecipeSection from "../components/AddRecipeSection";
+import { getAuth } from "firebase/auth";
 
 function Main() {
+  const auth = getAuth();
   return (
     <>
       <div className="main-container">
@@ -25,9 +27,15 @@ function Main() {
               <Link className="link" to="/recipes">
                 <button className="btn browse">Browse Recipes</button>
               </Link>
-              <Link to="/add-recipe" className="link">
-                <button className="btn add">Add Recipe</button>
-              </Link>
+              {auth.currentUser ? (
+                <Link to="/add-recipe" className="link">
+                  <button className="btn ">Add Recipe</button>
+                </Link>
+              ) : (
+                <Link to="/categories" className="link">
+                  <button className="btn ">Browse Categories</button>
+                </Link>
+              )}
             </div>
           </div>
         </header>
